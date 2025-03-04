a0cbe5977576754e4dd9f41ea998aa3867e47307
This.X <rockerzeeza@gmail.com>
2025-03-04 15:20:36 +0700

    remove unused page component; update tsconfig formatting; add new content page and middleware

--- app/content/[slug]/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-export default async function Page({
-  params,
-}: {
-  params: Promise<{ slug: string, id: string }>
-}) {
-  return <h1>My Page {(await params).slug} {(await params).id}</h1>
-}
---
+++ app/content/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ContentPage() {
+  return <h1>Content Page</h1>
+}
