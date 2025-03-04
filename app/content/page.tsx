7b41291b73035307e61588aaf74f3d16d338b471
This.X <rockerzeeza@gmail.com>
2025-03-04 16:28:56 +0700

    fetch and display blogs on content page; comment out middleware log

--- app/content/page.tsx
+++ app/content/page.tsx
@@ -1,5 +1,27 @@
-import React from "react";
+async function getBlogs() {
+  const res = await fetch('https://67c6b8eb351c081993fe7070.mockapi.io/blogs')
 
-export default function ContentPage() {
-  return <h1>Content Page</h1>
+  if (!res.ok) {
+    throw new Error('cannot fetch blog')
+  }
+  return res.json()
+
+}
+
+export default async function ContentPage() {
+
+  const blogs = await getBlogs()
+
+  return (
+    <>
+      <h1>Content Page</h1>
+      {
+        blogs.map((blog: any, index: any) => (
+          <div key={index}>
+            {blog.id}{blog.name}
+          </div>
+        ))
+      }
+    </>
+  )
 }
--- middleware.tsx
+++ middleware.tsx
@@ -3,7 +3,7 @@ import type { NextRequest } from 'next/server'
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-    console.log('test middleware')
+    // console.log('test middleware')
     return NextResponse.next()
 }
 
